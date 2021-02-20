4480d21f9b0a768469a235a107b2373a35fcdf17
eugenie9 <yavuuztuncil@gmail.com>
2021-02-21 01:12:11 +0300

    finding & visualization of conversion paths

--- binance.js
+++ binance.js
@@ -10,6 +10,7 @@ const account = new binanceClient().options({
 
 let markets = []
 let pairsInfo = []
+let pairs = []
 
 /**
  * Connects to the Binance API and fetches exchange info and trading pairs.
@@ -21,6 +22,11 @@ let initExchangeInfo = async () => {
       for (let obj of data.symbols) {
         if(obj.status!="TRADING") {
           continue
+        }
+        if(!pairs.includes(obj.baseAsset)) {
+          pairs.push(obj.baseAsset)
+        } else if(!pairs.includes(obj.quoteAsset)) {
+          pairs.push(obj.quoteAsset)
         }
         let filters = { status: obj.status }
         for (let filter of obj.filters) {
@@ -58,21 +64,86 @@ let initExchangeInfo = async () => {
   }
 }
 
+let ifMarketExists = (base, converted) => {
+  for(let pair in pairsInfo) {
+    if(pairsInfo[pair].tokenName == base && pairsInfo[pair].marketName == converted) {
+      return true
+    } else if(pairsInfo[pair].tokenName == converted && pairsInfo[pair].marketName ==base) {
+      return true
+    }
+  }
+  return false
+}
+
 let getTradingPairs = (askedPair) => {
   let array = []
   for(let pair in pairsInfo) {
-    if(pairsInfo[pair].tokenName == askedPair) {
+    if(pairsInfo[pair].tokenName == askedPair || pairsInfo[pair].marketName == askedPair) {
       array.push({tokenName: pairsInfo[pair].tokenName, marketName: pairsInfo[pair].marketName})
     }
   }
   return array
 }
 
+let matchTradingPairs = (base, converted) => {
+  let arr = []
+  for(let b of base) {
+    for(let c of converted) {
+      if(b.marketName == c.marketName || b.marketName == c.tokenName) {
+        arr.push([b, c])
+      } 
+    }
+  }
+
+  return arr
+}
+
+/**
+ * Only supports up to 3 step conversion
+ * @param {string} ba base asset
+ * @param {string} co converted asset
+ */
+let getConversionPath = (ba,co) => {
+  // If pairs cannot convert directly
+  if(!binanceEngine.ifMarketExists(ba,co)) {
+    let base = binanceEngine.getTradingPairs(ba)
+    let converted = binanceEngine.getTradingPairs(co)
+    let match = binanceEngine.matchTradingPairs(base,converted)
+    let integratedMatch = match
+    
+    // If pairs cannot convert within two step
+    if(match.length==0) {
+      integratedMatch = []
+      for(let b of base) {
+        if(binanceEngine.ifMarketExists(b.marketName, co)) {
+          integratedMatch.push([{tokenName: ba, marketName:b.marketName}, {tokenName: b.marketName, marketName: co}])
+          continue
+        }
+
+        let newBase = binanceEngine.getTradingPairs(b.marketName)
+        let newMatch = binanceEngine.matchTradingPairs(newBase, converted)
+        if(newMatch.length) {
+          for(let i=0; i<newMatch.length; i++) {
+            newMatch[i].unshift({tokenName: ba, marketName: b.marketName})
+          }
+          integratedMatch.push(newMatch[0])
+        }
+      }
+    } 
+    return integratedMatch
+  }
+  return [[{tokenName: ba, marketName: co}]]
+} 
+
 const binanceEngine = {
   markets,
   pairsInfo,
+  pairs,
   initExchangeInfo,
-  getTradingPairs
+  ifMarketExists,
+  getTradingPairs,
+  matchTradingPairs,
+  getConversionPath
 }
 
 module.exports = binanceEngine
--- main.js
+++ main.js
@@ -1,8 +1,38 @@
+const { createWebSocketStream } = require("ws")
 const binanceEngine = require("./binance")
+const express = require("express")
+const bodyParser = require("body-parser")
+const http_port = 3006
 
-let start = async() => {
+let initHttpServer = async() => {
+  console.log("fetching binance exchange")
   await binanceEngine.initExchangeInfo()
-  console.log(binanceEngine.getTradingPairs("BNB"))
-}
+  console.log("binance done")
+  const app = express();
 
-start()
+  app.set("view engine", "ejs");
+  app.set("views", (__dirname + "/views"));
+  app.use("/", express.static("public"));
+  app.use(express.static(__dirname + "/public"));
+  // app.use(bodyParser.urlencoded({ extended: true }));
+  // app.use(bodyParser.json());
+  // app.use(bodyParser.raw());
+  app.use(express.json())
+  app.use(express.urlencoded())
+
+  app.get("/", async(req,res) => {
+    res.render("index", {pairs: binanceEngine.pairs})
+  })
+
+  app.post("/findPath", async(req,res) => {
+    let {base, converted} = req.body
+    let match = binanceEngine.getConversionPath(base,converted)
+    res.send(JSON.stringify(match))
+  })
+
+  app.listen(http_port, () =>
+    console.log("Listening http on port: " + http_port)
+  );
+};
+
+initHttpServer()
